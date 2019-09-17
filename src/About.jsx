0d67b3aad2895d533e95beb1473483700ce17c06
AlexSaxena <saxenaalex@gmail.com>
2019-09-17 11:02:28 +0200

    Adds interaction with header by adding two new tabs, About me & Projects, also debug minor errors

---
+++ src/About.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const About = () => {
+    return (
+        <div className="ui container">
+            <h1 className="ui header">About Me</h1>
+            <p>Wazzap ma dude!</p>
+
+        </div>
+    )
+}
+
+export default About
---
+++ src/Projects.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Projects = () => {
+    return (
+        <div className="ui conatiner">
+            <h1 className="ui header">My Projects</h1>
+            <p>Here are some of my Projects!</p>
+        </div>
+    )
+};
+
+export default Projects
