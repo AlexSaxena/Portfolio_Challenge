6b39d50a433e6a703bb9f0ab076d3e6b2e935ec8
AlexSaxena <saxenaalex@gmail.com>
2019-09-17 15:39:43 +0200

    Adds illustrations using Undraw for Projects and About me Tabs.

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react"
 import axios from "axios"
 import ProjectCard from "./ProjectCard"
+import { UndrawDesigner } from 'react-undraw-illustrations';
 
 class Projects extends Component {
     constructor() {
@@ -34,7 +35,15 @@ class Projects extends Component {
 
         return(
             <div className="ui main container">
-                <h1 className="ui header">My Projects</h1>
+                <div className="ui stackable two column grid">
+                    <div className="column">
+                        <UndrawDesigner />
+                    </div>
+                    <div className="column">
+                        <h1 className="ui header">My Projects</h1>
+                        <p>Loads of cool information about different projects that I have worked on!</p>
+                    </div>
+                </div>
                 <div className="ui stackable four coloumn grid">
                 {projectsList}
             </div>
