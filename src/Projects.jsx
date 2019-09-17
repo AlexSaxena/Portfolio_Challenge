2fd3a64b5901b05c54e5e71ae3ceb6822948aba2
AlexSaxena <saxenaalex@gmail.com>
2019-09-17 14:04:28 +0200

    Adds Projects card to repsent projects

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,12 +1,50 @@
-import React from "react"
+import React, { Component } from "react"
+import axios from "axios"
+import ProjectCard from "./ProjectCard"
 
-const Projects = () => {
-    return (
-        <div className="ui conatiner">
-            <h1 className="ui header">My Projects</h1>
-            <p>Here are some of my Projects!</p>
+class Projects extends Component {
+    constructor() {
+        super();
+        this.state = {
+            projects: []
+        };
+    }
+    componentDidMount() {
+        axios.get('./src/data/projects.json')
+            .then(response => {
+                this.setState({
+                    projects: response.data
+                })
+            })
+    }
+
+    render() {
+        const projects = this.state.projects
+        let projectsList
+
+        if (projects.length > 0) {
+            projectsList = projects.map(project => {
+                return (
+                    <div key={project.id}>
+                        <ProjectCard project={project} />
+                    </div>
+                )
+            })
+        }
+
+        return(
+            <div className="ui main container">
+                <h1 className="ui header">My Projects</h1>
+                <div className="ui stackable four coloumn grid">
+                {projectsList}
+            </div>
         </div>
-    )
+        )
+    }
 };
+
+
+
+
 
 export default Projects
