591d1573dcfee9dfe043052e2ae40410b4f03904
AlexSaxena <saxenaalex@gmail.com>
2019-09-18 10:00:46 +0200

    Updates Hello and About section with pictures.

--- src/Hello.jsx
+++ src/Hello.jsx
@@ -1,14 +1,19 @@
 import React from "react"
-import { UndrawDesigner } from 'react-undraw-illustrations';
+import { UndrawBusinessPlan } from 'react-undraw-illustrations';
 
 const Hello = () => {
     return (
         <div className="ui main container">
-            <h1>
-                Hello World
-            </h1>
+            <div class="ui stackable two column grid">
+                <div class="column">
+                    <UndrawBusinessPlan />
+                </div>
+                <div class="column">
+                    <h1 className="ui header">My Portfolio</h1>
+                    <p> </p>
+                </div>
+            </div>
         </div>
-
     )
 }
 
